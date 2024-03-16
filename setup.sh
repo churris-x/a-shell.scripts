993a734d51075d8546650e642130cae16ae141ea
Francisco Altimari <francisco@altimari.nl>
2024-03-16 16:52:03 +0100

    Create symbolic link to repo dot files

--- setup.sh
+++ setup.sh
@@ -1,3 +1,22 @@
+# Create symlinks
+
+echo "$0 will create generic links to:"
+echo "    ~/Downloads/.profile"
+echo "    ~/Downloads/.aliases"
+
+cd ~/Documents
+
+# Only create symlink if it does note exist
+
+if [ ! -L .profile ]; then
+    ln -s .scripts/.profile .profile
+fi
+
+if [ ! -L .aliases ]; then
+    ln -s .scripts/.aliases .aliases
+fi
+
+# Configure appearance
 config -b #2d2a2e
 config -f #fcfcfa
 config -c #727072
