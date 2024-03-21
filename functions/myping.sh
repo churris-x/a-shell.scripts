19593ba2921e58dbf71ce1061a95eb0280e3def4
Francisco Altimari <francisco@altimari.nl>
2024-03-21 16:33:33 +0100

    Remove myping function

--- functions/myping.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Checks Internet connection
-
-# This is currently not working for some reason ¯\_(ツ)_/¯
-myping () {
-    ping 8.8.8.8 | while read -r pong; do echo "$(date): $pong"; done
-}
-
-myping
