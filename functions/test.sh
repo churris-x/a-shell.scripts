3d7559cdfbe53b7fea5857f6b567f3afad24789a
Francisco Altimari <francisco@altimari.nl>
2024-03-21 15:42:01 +0100

    Test function

--- functions/test.sh
+++ functions/test.sh
@@ -1,6 +1,13 @@
 echo 'testing script'
 
-lg2 branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* master/(master)/' -e 's/* \(.*\)/(__\1__)/'
+# parse_git_branch () {
+#     lg2 branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* master/(master)/' -e 's/* \(.*\)/(__\1__)/'
+# }
 
+comeonfortheloveofgod () {
+    ping 8.8.8.8
+}
+
+comeonfortheloveofgod
 
 echo 'fin'
