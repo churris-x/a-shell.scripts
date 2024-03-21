39632d89b9414431093a3f67317954cac8dc3870
Francisco Altimari <francisco@altimari.nl>
2024-03-21 16:04:27 +0100

    Test different function invocation

--- functions/test.sh
+++ functions/test.sh
@@ -1,5 +1,6 @@
 echo 'testing script'
 
+# impossible to parse the current git branch name because libgit is old and garbage :D
 # parse_git_branch () {
 #     lg2 branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* master/(master)/' -e 's/* \(.*\)/(__\1__)/'
 # }
@@ -9,5 +10,16 @@ comeonfortheloveofgod () {
 }
 
 comeonfortheloveofgod
+
+echo ''
+echo 'spacing'
+echo ''
+
+
+shouldnotbethisdifficult () {
+    ping 8.8.8.8 | while read -r pong; do echo "$(date): $pong"; done
+}
+
+shouldnotbethisdifficult
 
 echo 'fin'
