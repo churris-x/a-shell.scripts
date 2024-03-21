14e511d4daafc0eb07fb48c14957d29e2f402b9f
Francisco Altimari <francisco@altimari.nl>
2024-03-21 15:32:46 +0100

    Remove functions altogether
    I'm begining to go crazy here, give me a break

--- functions/myping.sh
+++ functions/myping.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # Checks Internet connection
-myping () {
-    ping 8.8.8.8 | while read pong; do echo "$(date): $pong"; done
-}
-myping
+
+ping 8.8.8.8 | while read pong; do echo "$(date): $pong"; done
+
--- functions/test.sh
+++ functions/test.sh
@@ -1,9 +1,6 @@
 echo 'testing script'
 
-parse_git_branch () {
-    lg2 branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* master/(master)/' -e 's/* \(.*\)/(__\1__)/'
-}
+lg2 branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* master/(master)/' -e 's/* \(.*\)/(__\1__)/'
 
-parse_git_branch
 
 echo 'fin'
